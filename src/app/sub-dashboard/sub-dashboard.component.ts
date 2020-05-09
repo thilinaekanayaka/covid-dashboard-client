6ff481dc9cb16692851a35ea1eef2000e40e1d1d
Thilina Ekanayaka <Thilina>
2020-05-09 17:45:54 +0530

    sub dashboard UI

--- src/app/sub-dashboard/sub-dashboard.component.ts
+++ src/app/sub-dashboard/sub-dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Districts } from '../districts';
 import { SyncService } from '../sync.service';
+import { Statuses } from '../status';
 
 @Component({
   selector: 'app-sub-dashboard',
@@ -12,9 +13,11 @@ export class SubDashboardComponent implements OnInit {
   cases: any;
   districtID: any;
   district: any;
+  statuses: any;
 
   constructor(private route: ActivatedRoute, private syncService: SyncService) {
     this.districtID = this.route.snapshot.paramMap.get('id');
+    this.statuses = Statuses;
   }
 
   ngOnInit(): void {
